1c0208c2e8f73295a41d9daee4207cad8ceea97d
Gatuk S. Chattanon <gatukgl@gmail.com>
2022-07-24 22:15:03 +0700

    Show default ads image to main page

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import image from '../example-ads.jpg';
+</script>
+
+<div class="container mx-auto">
+	<h1 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+		Thurstech Ads Generator
+	</h1>
+
+	<slot name="ads">
+		<img src={image} width="1000px" alt="ThursTech Ads Template" />
+	</slot>
+</div>
